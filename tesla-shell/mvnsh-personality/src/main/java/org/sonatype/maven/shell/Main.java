9c5b359b0d3dc157a76497577297109a5450d3d5
Jason van Zyl <jason@sonatype.com>
2011-07-14 10:23:44 +0200

    o snapshot of adding the shell

---
+++ tesla-shell/mvnsh-personality/src/main/java/org/sonatype/maven/shell/Main.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2011 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at 
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * The Apache License v2.0 is available at
+ *   http://www.apache.org/licenses/LICENSE-2.0.html
+ * You may elect to redistribute this code under either of these licenses. 
+ *******************************************************************************/
+
+package org.sonatype.maven.shell;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Stage;
+import com.google.inject.name.Names;
+import org.sonatype.gshell.MainSupport;
+import org.sonatype.gshell.branding.Branding;
+import org.sonatype.gshell.command.registry.CommandRegistrar;
+import org.sonatype.gshell.command.IO;
+import org.sonatype.gshell.console.ConsoleErrorHandler;
+import org.sonatype.gshell.console.ConsolePrompt;
+import org.sonatype.gshell.guice.CoreModule;
+import org.sonatype.gshell.guice.GuiceMainSupport;
+import org.sonatype.gshell.logging.LoggingSystem;
+import org.sonatype.gshell.logging.gossip.GossipLoggingSystem;
+import org.sonatype.gshell.shell.Shell;
+import org.sonatype.gshell.shell.ShellErrorHandler;
+import org.sonatype.gshell.shell.ShellImpl;
+import org.sonatype.gshell.shell.ShellPrompt;
+import org.sonatype.gshell.variables.Variables;
+
+import java.util.List;
+
+/**
+ * Command-line bootstrap for Apache Maven Shell (<tt>mvnsh</tt>).
+ * 
+ * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ * @since 0.7
+ */
+public class Main
+    extends GuiceMainSupport
+{
+    @Override
+    protected Branding createBranding() {
+        return new BrandingImpl();
+    }
+
+    @Override
+    protected void configure(final List<Module> modules) {
+        super.configure(modules);
+
+        Module custom = new AbstractModule()
+        {
+            @Override
+            protected void configure() {
+                bind(LoggingSystem.class).to(GossipLoggingSystem.class);
+                bind(ConsolePrompt.class).to(ShellPrompt.class);
+                bind(ConsoleErrorHandler.class).to(ShellErrorHandler.class);
+            }
+        };
+
+        modules.add(custom);
+    }
+
+    public static void main(final String[] args) throws Exception {
+        new Main().boot(args);
+    }
+}
