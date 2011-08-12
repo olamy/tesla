0cd5626ace7282156ddc4f9b79b9b59264b368d3
Jason van Zyl <jason@sonatype.com>
2011-08-12 17:04:50 -0400

    o see if this bootstrap will work in hudson

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+ANT=`which ant`
+
+[ -z ${ANT} ] && ANT=/var/lib/hudson/tools/Ant_1.8.2/bin/ant
+
+echo "Using ${ANT}"
+
 distributionId="tesla"
 distributionShortName="Tesla"
-distributionName="Sonatype Tesla"
+distributionName="Eclipse Tesla"
 
-ant \
+${ANT} \
   -Dmaven.home=${HOME}/tesla-3.0.4-SNAPSHOT \
   -DdistributionId="${distributionId}" \
   -DdistributionShortName="${distributionShortName}" \
-  -DdistributionName="${distributionName}"
+  -DdistributionName="${distributionName}" \
+  -DhostEnvSettings="${HOME}/.m2/settings.xml"
